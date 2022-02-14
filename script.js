9812f4caa14ea55ab771466796dba3deceb4d444
Fernando Mercado <fermercado@live.com>
2022-02-14 19:44:21 -0300

    Init Tab Nav

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li')
+  const tabContent = document.querySelectorAll('.js-tabcontent section')
 
-function activeTab(index) {
-  tabContent[index].classList.add('ativo')
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo')
+
+    function activeTab(index) {
+      tabContent.forEach(section => {
+        section.classList.remove('ativo')
+      })
+      tabContent[index].classList.add('ativo')
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index)
+      })
+    })
+  }
 }
+initTabNav()
