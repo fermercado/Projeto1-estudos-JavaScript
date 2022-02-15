aac0e446065451947b39e4919e1132bcd8df3f53
Fernando Mercado <fermercado@live.com>
2022-02-14 21:10:52 -0300

    Accordion list

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ function initTabNav() {
   }
 }
 initTabNav()
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  const activeClass = 'ativo'
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass)
+    accordionList[0].nextElementSibling.classList.add(activeClass)
+    function activeAccordion() {
+      this.classList.toggle(activeClass)
+      this.nextElementSibling.classList.toggle(activeClass)
+    }
+    accordionList.forEach(item => {
+      item.addEventListener('click', activeAccordion)
+    })
+  }
+}
+
+initAccordion()
