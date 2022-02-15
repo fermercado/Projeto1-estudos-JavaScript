2787f2d9e33fe9231e9bb032b319b7e792aabd79
Fernando Mercado <fermercado@live.com>
2022-02-15 12:33:39 -0300

    Scroll suave e animação

--- script.js
+++ script.js
@@ -38,3 +38,50 @@ function initAccordion() {
 }
 
 initAccordion()
+
+function initScrollSmooth() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+
+    // forma alternativa
+    // const topo = section.offsetTop
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth'
+    // })
+  }
+  linksInternos.forEach(link => {
+    link.addEventListener('click', scrollToSection)
+  })
+}
+
+initScrollSmooth()
+
+function initAnimationScroll() {
+  const sections = document.querySelectorAll('.js-scroll')
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.7
+
+    function animaScroll() {
+      sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top
+        const isSectionVisible = sectionTop - windowMetade < 0
+        if (isSectionVisible) section.classList.add('ativo')
+        // else section.classList.remove('ativo')
+      })
+    }
+  }
+  animaScroll()
+  window.addEventListener('scroll', animaScroll)
+}
+initAnimationScroll()
